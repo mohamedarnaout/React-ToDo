91af29608dde2629b443b5705a6cca37f329ee64
“mohamed <[mohamedearna@gmail.com]>
2022-12-29 11:40:09 -0600

    finished project

--- src/Components/Task/task.jsx
+++ src/Components/Task/task.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import check from "./60731.png";
+import trash from "./big-trash.png";
 function Task(props) {
   const [isActive, setIsActive] = useState(false);
 
@@ -7,16 +8,25 @@ function Task(props) {
     setIsActive(!isActive);
     console.log(isActive);
   };
+
   return (
     <div>
-      <div className="flex task">
-        <button
-          className={isActive ? "checked" : "check"}
-          onClick={handleClick}
-        >
-          {isActive ? <img src={check} alt="" className="checkmark" /> : ""}
-        </button>
-        <h4 className="task-text"> {props.name}</h4>
+      <div className="flex-end  task">
+        <div className="flex">
+          <button
+            className={isActive ? "checked" : "check"}
+            onClick={handleClick}
+          >
+            {isActive ? <img src={check} alt="" className="checkmark" /> : ""}
+          </button>
+          <h4 className="task-text"> {props.name}</h4>
+        </div>
+
+        <div>
+          <button className="task-delete" onClick={props.delete}>
+            <img src={trash} alt="" className="trash" />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Components/Tasks/tasks.jsx
+++ src/Components/Tasks/tasks.jsx
@@ -3,7 +3,12 @@ import Task from "../Task/task";
 import "./task.css";
 
 function Tasks(props) {
-  const listTasks = props.arr.map((task) => <Task name={task} />);
+  const deleteClick = (e) => {
+    e.target.parentNode.parentNode.parentNode.parentNode.style.display = "none";
+  };
+  const listTasks = props.arr.map((task) => (
+    <Task name={task} delete={deleteClick} />
+  ));
   return <div className="tasks">{listTasks}</div>;
 }
 
